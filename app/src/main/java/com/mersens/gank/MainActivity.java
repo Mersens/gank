0384718bcf4b0b22bd960649c6e7eb3ac84a82b9
Mersens <626168564@qq.com>
2016-11-07 17:35:12 +0800

    Initial commit

---
+++ app/src/main/java/com/mersens/gank/MainActivity.java
@@ -0,0 +1,108 @@
+package com.mersens.gank;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private List<String> tabTitles;
+    private TabLayout mTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        initDatas();
+    }
+
+
+    public void initDatas(){
+        tabTitles = new ArrayList<String>();
+        tabTitles.add("Android");
+        tabTitles.add("IOS");
+        tabTitles.add("福利");
+        tabTitles.add("前端");
+        tabTitles.add("休息视频");
+        tabTitles.add("拓展资源");
+        mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles.get(0)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles.get(1)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles.get(2)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles.get(3)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles.get(4)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles.get(5)));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_camera) {
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
